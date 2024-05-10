cbefb9c771b70bf677a93d1d68ef68aee665c95d
Alma Liezenga <alma.liezenga@gmail.com>
2024-05-10 16:50:08 +0200

    Adding BechdelTestAPI

---
+++ src/python_ingest/BechdelTestAPI.py
@@ -0,0 +1,24 @@
+import requests
+import pandas as pd
+import logging
+import json
+
+# use the @data_loader decorator to materialize an asset
+# make sure you have a corresponding .yml table definition matching the function's name
+# for more information check out our docs: https://www.y42.com/docs/sources/ingest-data-using-python
+
+@data_loader
+def BechdelTestAPI(context) -> pd.DataFrame:
+    # Your code goes here
+    url = "http://bechdeltest.com/api/v1/getAllMovies"
+    response = requests.get(url)
+    data = json.loads(response.content)
+
+    # Return a DataFrame which will be materialized within your data warehouse
+    df = pd.DataFrame(data)
+
+    logging.info("Data fetched and DataFrame created successfully.")
+
+    # to learn how to set up incremental updates and more
+    # please visit https://www.y42.com/docs/sources/ingest-data-using-python
+    return df
