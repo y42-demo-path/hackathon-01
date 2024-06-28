6e3fd44fda1809af9956702ca50e0dd4ef73b44d
Alma Liezenga <alma.liezenga@gmail.com>
2024-06-28 15:15:59 +0200

    Reformatted staging Bechdel test input

--- src/models/staging/stg_input_form_bechdel_test__Formulierreacties_1.sql
+++ src/models/staging/stg_input_form_bechdel_test__Formulierreacties_1.sql
@@ -7,10 +7,15 @@ source AS (
 renamed AS (
 	SELECT
 		"MOVIE_IMDBID_" AS imdb_id,
-		"BECHDEL_TEST_SCORE" AS new_bechdel_rating,
+		CASE
+			WHEN "BECHDEL_TEST_SCORE" = 'This movie does not have two (named) female characters.' THEN '0'
+			WHEN "BECHDEL_TEST_SCORE" = 'It has at least two women in it;' THEN '1'
+			WHEN "BECHDEL_TEST_SCORE" = '... and they talk to each other;' THEN '2'
+			WHEN "BECHDEL_TEST_SCORE" = '... about something besides a man.' THEN '3'
+		END AS new_bechdel_rating,
 		"YOUR_COMMENT" AS bechdel_comment, 
-		IFF(LEN("NAME_OR_HANDLE" > 0), "NAME_OR_HANDLE", 'Alma') AS submitter_name, 
-		"E_MAIL_ADDRESS" AS submitter_e_mail
+		IFF(LEN("NAME_OR_HANDLE") > 0, "NAME_OR_HANDLE", 'Alma') AS submitter_name, 
+		IFF(LEN("E_MAIL_ADDRESS") > 0, "E_MAIL_ADDRESS", 'alma.liezenga@gmail.com') AS submitter_e_mail 
 	FROM source
 )
 
