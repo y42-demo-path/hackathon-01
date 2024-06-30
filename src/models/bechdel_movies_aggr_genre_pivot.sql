050dc22deb6c24144b4846f5c08959172453f497
Alma Liezenga <alma.liezenga@gmail.com>
2024-06-30 16:17:22 +0200

    Correct pivot tables

--- src/models/bechdel_movies_aggr_genre_pivot.sql
+++ src/models/bechdel_movies_aggr_genre_pivot.sql
@@ -4,6 +4,31 @@ b_movies AS (
 	SELECT * FROM {{ ref('bechdel_movies_aggr_genre_bechdelrating') }}
 ),
 
-pivot AS (
-    select 
+pivotted AS (
+    SELECT 
+        FIRST_GENRE,
+        total_movies, 
+        bechdel_pass_cat, 
+        bechdel_pass_perc
+    FROM b_movies
+    unpivot (
+		bechdel_pass_perc
+		for bechdel_pass_cat in (bechdel_pass_perc_0, bechdel_pass_perc_1, bechdel_pass_perc_2, bechdel_pass_perc_3)
 )
+),
+
+renamed AS (
+    SELECT 
+        FIRST_GENRE, 
+        total_movies,
+        CASE
+            WHEN bechdel_pass_cat = 'BECHDEL_PASS_PERC_0' THEN 'This movie does not have two (named) female characters.'
+			WHEN bechdel_pass_cat = 'BECHDEL_PASS_PERC_1' THEN 'This movie has at least two women in it.'
+			WHEN bechdel_pass_cat = 'BECHDEL_PASS_PERC_2' THEN 'This movie has at least two women in it and they talk to each other.'
+			WHEN bechdel_pass_cat = 'BECHDEL_PASS_PERC_3' THEN 'This movie has at least two women in it and they talk to each other about something besides a man.'
+		END AS bechdel_category, 
+        bechdel_pass_perc
+    FROM pivotted
+)
+		
+select * from renamed
--- src/models/movies_bechdel_aggr_year_pivot.sql
+++ src/models/movies_bechdel_aggr_year_pivot.sql
@@ -1,29 +1,34 @@
 WITH 
 
 b_movies AS (
-	SELECT * FROM {{ ref('bechdel_movies') }}
+	SELECT * FROM {{ ref('movies_bechdel_aggr_year') }}
 ),
 
-aggregated AS (
-	SELECT
-		FIRST_GENRE, 
-		COUNT(*) AS total_movies,
-		SUM(CASE WHEN bechdel_rating = 0 THEN 1 ELSE 0 END) AS bechdel_pass_0, 
-		SUM(CASE WHEN bechdel_rating = 1 THEN 1 ELSE 0 END) AS bechdel_pass_1, 
-		SUM(CASE WHEN bechdel_rating = 2 THEN 1 ELSE 0 END) AS bechdel_pass_2,
-		SUM(CASE WHEN bechdel_rating = 3 THEN 1 ELSE 0 END) AS bechdel_pass_3, 
-		bechdel_pass_0 / total_movies AS bechdel_pass_perc_0,
-		bechdel_pass_1 / total_movies AS bechdel_pass_perc_1,
-		bechdel_pass_2 / total_movies AS bechdel_pass_perc_2,
-		bechdel_pass_3 / total_movies AS bechdel_pass_perc_3
-	FROM 
-    	b_movies
-	GROUP BY 
-    	FIRST_GENRE
-	ORDER BY 
-    	total_movies
+pivotted AS (
+    SELECT 
+        "year",
+        total_movies, 
+        bechdel_pass_cat, 
+        bechdel_pass_perc
+    FROM b_movies
+    unpivot (
+		bechdel_pass_perc
+		for bechdel_pass_cat in (bechdel_pass_perc_0, bechdel_pass_perc_1, bechdel_pass_perc_2, bechdel_pass_perc_3)
 )
+),
 
-
-
-select * from aggregated
+renamed AS (
+    SELECT 
+        "year", 
+        total_movies,
+        CASE
+            WHEN bechdel_pass_cat = 'BECHDEL_PASS_PERC_0' THEN 'This movie does not have two (named) female characters.'
+			WHEN bechdel_pass_cat = 'BECHDEL_PASS_PERC_1' THEN 'This movie has at least two women in it.'
+			WHEN bechdel_pass_cat = 'BECHDEL_PASS_PERC_2' THEN 'This movie has at least two women in it and they talk to each other.'
+			WHEN bechdel_pass_cat = 'BECHDEL_PASS_PERC_3' THEN 'This movie has at least two women in it and they talk to each other about something besides a man.'
+		END AS bechdel_category, 
+        bechdel_pass_perc
+    FROM pivotted
+)
+		
+select * from renamed
