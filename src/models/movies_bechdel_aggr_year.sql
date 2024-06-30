160e85a9a168ea65f8cfcbeecee727cf911e3296
Alma Liezenga <alma.liezenga@gmail.com>
2024-06-30 14:30:39 +0200

    alter bechdel test per year table to include all categories

--- src/models/movies_bechdel_aggr_year.sql
+++ src/models/movies_bechdel_aggr_year.sql
@@ -8,7 +8,14 @@ aggregated AS (
 	SELECT
 		"year",
 		COUNT(*) AS total_movies,
-		SUM(CASE WHEN bechdel_rating = 3 THEN 1 ELSE 0 END) / total_movies AS bechdel_pass_perc
+		SUM(CASE WHEN bechdel_rating = 0 THEN 1 ELSE 0 END) AS bechdel_pass_0, 
+		SUM(CASE WHEN bechdel_rating = 1 THEN 1 ELSE 0 END) AS bechdel_pass_1, 
+		SUM(CASE WHEN bechdel_rating = 2 THEN 1 ELSE 0 END) AS bechdel_pass_2,
+		SUM(CASE WHEN bechdel_rating = 3 THEN 1 ELSE 0 END) AS bechdel_pass_3, 
+		bechdel_pass_0 / total_movies AS bechdel_pass_perc_0,
+		bechdel_pass_1 / total_movies AS bechdel_pass_perc_1,
+		bechdel_pass_2 / total_movies AS bechdel_pass_perc_2,
+		bechdel_pass_3 / total_movies AS bechdel_pass_perc_3
 	FROM 
     	b_movies
 	GROUP BY 
