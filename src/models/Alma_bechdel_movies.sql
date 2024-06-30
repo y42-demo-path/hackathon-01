48f2495b06065d75461d26746848301d96b7136a
Alma Liezenga <alma.liezenga@gmail.com>
2024-06-30 20:44:30 +0200

    Set up staging for new input form

---
+++ src/models/Alma_bechdel_movies.sql
@@ -0,0 +1,29 @@
+WITH 
+
+s_movies AS (
+	SELECT * FROM {{ ref('all_movies') }}
+),
+
+s_input AS (
+	SELECT * FROM {{ ref('stg_input_form_bechdel_test__Formulierreacties_1') }}
+),
+
+joined AS (
+	SELECT
+		si.imdb_id,
+		{# sm.bechdel_rating,
+		sm.bechdel_rating_description,
+		sm.imdb_avg_rating,
+		sm.num_votes_imdb_rating,
+		sm.first_genre, 
+		sm.all_genres,
+		sm."budget",
+		sm."gross" #}
+	FROM 
+    	s_input si
+    WHERE SUBMITTER_NAME = 'Alma'
+	{# LEFT JOIN s_movies AS sm
+		ON si.imdb_id = sm.imdb_id #}
+)
+
+select * from joined
--- src/models/staging/stg_input_form_bechdel_test__Formulierreacties_1.sql
+++ src/models/staging/stg_input_form_bechdel_test__Formulierreacties_1.sql
@@ -7,15 +7,8 @@ source AS (
 renamed AS (
 	SELECT
 		"MOVIE_IMDBID_" AS imdb_id,
-		CASE
-			WHEN "BECHDEL_TEST_SCORE" = 'This movie does not have two (named) female characters.' THEN '0'
-			WHEN "BECHDEL_TEST_SCORE" = 'It has at least two women in it;' THEN '1'
-			WHEN "BECHDEL_TEST_SCORE" = '... and they talk to each other;' THEN '2'
-			WHEN "BECHDEL_TEST_SCORE" = '... about something besides a man.' THEN '3'
-		END AS new_bechdel_rating,
-		"YOUR_COMMENT" AS bechdel_comment, 
-		IFF(LEN("NAME_OR_HANDLE") > 0, "NAME_OR_HANDLE", 'Alma') AS submitter_name, 
-		IFF(LEN("E_MAIL_ADDRESS") > 0, "E_MAIL_ADDRESS", 'alma.liezenga@gmail.com') AS submitter_e_mail 
+		"NAME_OR_HANDLE" AS name, 
+		"E_MAIL_ADDRESS" AS e_mail 
 	FROM source
 )
 
