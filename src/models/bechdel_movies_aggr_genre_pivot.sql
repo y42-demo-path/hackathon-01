4a02d71df3decb5110780a1d3a15bf47d77f4390
Alma Liezenga <alma.liezenga@gmail.com>
2024-06-30 16:22:51 +0200

    Make pivot prettier

--- src/models/bechdel_movies_aggr_genre_pivot.sql
+++ src/models/bechdel_movies_aggr_genre_pivot.sql
@@ -9,11 +9,11 @@ pivotted AS (
         FIRST_GENRE,
         total_movies, 
         bechdel_pass_cat, 
-        bechdel_pass_perc
+        bechdel_pass
     FROM b_movies
     unpivot (
-		bechdel_pass_perc
-		for bechdel_pass_cat in (bechdel_pass_perc_0, bechdel_pass_perc_1, bechdel_pass_perc_2, bechdel_pass_perc_3)
+		bechdel_pass
+		for bechdel_pass_cat in (bechdel_pass_0, bechdel_pass_1, bechdel_pass_2, bechdel_pass_3)
 )
 ),
 
@@ -22,13 +22,14 @@ renamed AS (
         FIRST_GENRE, 
         total_movies,
         CASE
-            WHEN bechdel_pass_cat = 'BECHDEL_PASS_PERC_0' THEN 'This movie does not have two (named) female characters.'
-			WHEN bechdel_pass_cat = 'BECHDEL_PASS_PERC_1' THEN 'This movie has at least two women in it.'
-			WHEN bechdel_pass_cat = 'BECHDEL_PASS_PERC_2' THEN 'This movie has at least two women in it and they talk to each other.'
-			WHEN bechdel_pass_cat = 'BECHDEL_PASS_PERC_3' THEN 'This movie has at least two women in it and they talk to each other about something besides a man.'
+            WHEN bechdel_pass_cat = 'BECHDEL_PASS_0' THEN 'This movie does not have two (named) female characters.'
+			WHEN bechdel_pass_cat = 'BECHDEL_PASS_1' THEN 'This movie has at least two women in it.'
+			WHEN bechdel_pass_cat = 'BECHDEL_PASS_2' THEN 'This movie has at least two women in it and they talk to each other.'
+			WHEN bechdel_pass_cat = 'BECHDEL_PASS_3' THEN 'This movie has at least two women in it and they talk to each other about something besides a man.'
 		END AS bechdel_category, 
-        bechdel_pass_perc
+        bechdel_pass
     FROM pivotted
+    ORDER BY total_movies, first_genre
 )
 		
 select * from renamed
