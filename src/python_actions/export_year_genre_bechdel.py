0e147eec2987e40c00f0304fe675955f35d6c6a7
Alma Liezenga <alma.liezenga@gmail.com>
2024-06-30 14:54:10 +0200

    set up export to google sheets

--- src/python_actions/export_year_genre_bechdel.py
+++ src/python_actions/export_year_genre_bechdel.py
@@ -18,14 +18,14 @@ def export_to_sheet(context, assets):
 
     # load credentials & authorize
     SCOPES = ('https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive')
-    service_account_info = json.loads(context.secrets.get('LOREM')
+    service_account_info = json.loads(context.secrets.get('Google service account'))
     my_credentials = service_account.Credentials.from_service_account_info(service_account_info, scopes=SCOPES)
     
     client = gspread.authorize(my_credentials)
     logging.info('authorized')
 
     # connect to sheet by ID and sync data
-    sht1 = client.open_by_key('IPSUM')
+    sht1 = client.open_by_key('10k0VO6cSJoVHStYpztBccjI_KAUYUsjOtTHLfm1wEME')
     worksheet = sht1.get_worksheet(0)
     logging.info('connected to sheet')
     data = [df.columns.to_numpy().tolist()] + df.to_numpy().tolist() # this works when all columns are of type string
